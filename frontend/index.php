6fb7c6736e0898e9565c48b17eb8833523f43a57
Admin <nguyenminhnguyet137@gmail.com>
2020-09-21 17:55:44 +0700

    thay doi thu muc theem file

---
+++ frontend/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Monnie Store - Trang chủ</title>
+    <?php include_once(__DIR__.'/../backend/layouts/styles.php'); ?>
+</head>
+<body>
+<div class="container-fluid ">
+    <!-- start header  -->
+    <?php include_once(__DIR__.'/../backend/layouts/partials/header.php'); ?>
+    <!-- end header  -->
+    <!-- start hinh index -->
+    <?php include_once(__DIR__.'/../backend/layouts/partials/hinh_index.php'); ?>
+    <!-- end hinh index  -->
+</div>
+    <?php include_once(__DIR__.'/../backend/layouts/scripts.php'); ?>
+</body>
+</html>
